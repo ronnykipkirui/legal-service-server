f66b60f5a8928380d4b58af94ba92826fd0ca794
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-12-28 23:37:39 +0300

    LS-15 <created dispute categories controller>

---
+++ app/controllers/dispute_categories_controller.rb
@@ -0,0 +1,16 @@
+class DisputeCategoriesController < ApplicationController
+    def index
+        cat = DisputeCategory.all
+        render json: cat, status: :ok
+        
+    end
+
+
+    def create
+        cat = DisputeCategory.create(category_name: params[:category_name])
+        render json: cat, status: :created
+    end
+
+
+    
+end

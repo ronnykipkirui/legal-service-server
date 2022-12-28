d88522beb98d5b3ad17b09d23de6f9f1ac4286cd
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-12-28 23:32:45 +0300

    LS-15 <created client model>

---
+++ app/models/client.rb
@@ -0,0 +1,12 @@
+class Client < ApplicationRecord
+    has_secure_password
+    
+    has_many :disputes
+    has_many :advocates, through: :disputes
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :phone, presence: true
+    
+
+end

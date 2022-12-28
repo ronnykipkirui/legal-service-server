88bd4f2264486496f59ac03570e23783ef300529
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-12-28 23:38:10 +0300

    LS-15 <created dispute types controller>

---
+++ app/controllers/dispute_types_controller.rb
@@ -0,0 +1,14 @@
+class DisputeTypesController < ApplicationController
+    def index
+        type = DisputeType.all
+        render json: type, status: :ok
+        
+    end
+
+    def create
+        type = DisputeType.create!(dispute_category_id: params[:dispute_category_id], dispute_name: params[:dispute_name])
+        render json: type, status: :created
+        
+    end
+   
+end

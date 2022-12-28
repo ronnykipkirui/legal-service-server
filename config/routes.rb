1ab3677e2594bbbecc44213a8471b799e9c1c4db
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-12-28 23:43:54 +0300

    LS-15 <created routes>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  # post "/signup", to: "clients#create"
+  resources :clients, only:[:index, :show, :create]
+  
+  # waiting on serializer
+  # post "/login", to: "sessions#create"
+  get "/dispute_categories", to: "dispute_categories#index"
+  resources :dispute_categories, only: [:create]
+  resources :dispute_types, only:[:index, :create]
+  
+  
+  # post "/advocates/signup", to: "advocates#create"
+  resources :advocates, only:[:index, :show, :create]
+  # post "/advocates/login", to: "sessions#create"
+  
+  resources :disputes, only: [:create, :index, :show, :destroy]
 end
+
